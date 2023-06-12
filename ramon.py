80fc13977bcd2166bec6b455a74fee2a4074fe03
Ramon Martins Alves <112656355+ramon-marrtins@users.noreply.github.com>
2023-06-12 10:39:26 -0300

    Update ramon.py

--- ramon.py
+++ ramon.py
@@ -1,3 +1,11 @@
 # Selenium
 
-import
+import cv2.data
+import numpy as np
+import cv2 as cv
+from selenium import webdriver
+from selenium.webdriver.common.keys import keys
+from selenium.webdriver.common.by import by
+
+from selunium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import GeckoDriveerManager
