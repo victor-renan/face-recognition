3c6441ba90095614a68f944c9a194ae493541235
Ramon Martins Alves <112656355+ramon-marrtins@users.noreply.github.com>
2023-06-13 15:41:04 -0300

    Update ramon.py

--- ramon.py
+++ ramon.py
@@ -1,9 +1,1 @@
 # Selenium
-
-from selenium import webdriver
-from selenium.webdriver.common.keys import keys
-
-driver = webdriver.Firefox(executable_path="")
-driver.get("https://www.google.com.br")
-driver.find_element_by_name("q").send_keys("Reconhecimento Facial")
-driver.find_element_by_name("q").send_keys(Keys.RETURN)
