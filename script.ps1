e69dba50fc3dcda6d2b44f8e1375b5e811128166
Renan Alves <86895147+victor-renan@users.noreply.github.com>
2023-06-13 18:38:37 -0300

    fix(): script atualizado

--- script.ps1
+++ script.ps1
@@ -1,3 +1,5 @@
-venv/Scripts/activate;
-pip install -r ./requirements.txt;
+python -m pip install virtualenv;
+python -m virtualenv .venv;
+.venv/Scripts/activate;
+python -m pip install -r ./requirements.txt;
 code .;
